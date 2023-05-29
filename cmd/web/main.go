0362152abe562e24800b1c592cc6f5cbf2a085ff
shahinm95 <shahin.movazzaf@gmail.com>
2023-05-29 17:52:56 +0330

    added calendar funcs and handlers part 3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -68,6 +68,7 @@ func run () (*driver.DB, error) {
 	gob.Register(models.Reservation{})
 	gob.Register(models.Room{})
 	gob.Register(models.RoomRestriction{})
+	gob.Register(map[string]int{})
 
 	mailChan := make(chan models.MailData)
 	app.MailChan = mailChan
--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -155,3 +155,9 @@ func (m *testDBRepo) AllRooms() ([]models.Room , error) {
 	var rooms []models.Room
 	return rooms , nil
 } 
+
+// GetRestrictionsForRoomByDate returns room restriction by given date
+func (m *testDBRepo) GetRestrictionsForRoomByDate(roomID int, start, end time.Time) ([]models.RoomRestriction, error){
+	var restrictions []models.RoomRestriction
+	return restrictions, nil
+}
